e1dc1c4a40708e751cc050f3d8e568e6a48050d0
VerdantE1 <2523877046@qq.com>
2025-03-10 14:43:54 +0800

    初始化项目

--- models/PostTags.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type PostTag struct {
-	ID     int `gorm:"primaryKey;comment:'文章与标签的关系标识'" json:"id"`
-	PostID int `gorm:"comment:'文章ID'" json:"post_id"`
-	TagID  int `gorm:"comment:'标签ID'" json:"tag_id"`
-}
--- models/Settings.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Setting struct {
-	ID    int    `gorm:"primaryKey;comment:'设置唯一标识'" json:"id"`
-	Key   string `gorm:"type:varchar(255);comment:'配置项名称'" json:"key"`
-	Value string `gorm:"type:text;comment:'配置项值'" json:"value"`
-}
