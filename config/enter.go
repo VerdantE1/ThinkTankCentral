e2e0c86b38eba7e6f1b7260f3ca9292da597e91f
VerdantE1 <2523877046@qq.com>
2025-03-18 17:47:27 +0800

    - 更新config

--- config/enter.go
+++ config/enter.go
@@ -2,11 +2,16 @@ package config
 
 type Config struct {
 	Captcha Captcha `json:"captcha" yaml:"captcha"`
-	Mysql   Mysql   `json:"mysql" yaml:"mysql"`
-	Zap     Zap     `json:"zap" yaml:"zap"`
-	System  System  `json:"system" yaml:"system"`
-	ES      ES      `json:"es" yaml:"es"`
 	Email   Email   `json:"email" yaml:"email"`
-	Redis   Redis   `json:"redis" yaml:"redis"`
+	ES      ES      `json:"es" yaml:"es"`
+	Gaode   Gaode   `json:"gaode" yaml:"gaode"`
 	Jwt     Jwt     `json:"jwt" yaml:"jwt"`
+	Mysql   Mysql   `json:"mysql" yaml:"mysql"`
+	Qiniu   Qiniu   `json:"qiniu" yaml:"qiniu"`
+	QQ      QQ      `json:"qq" yaml:"qq"`
+	Redis   Redis   `json:"redis" yaml:"redis"`
+	System  System  `json:"system" yaml:"system"`
+	Upload  Upload  `json:"upload" yaml:"upload"`
+	Website Website `json:"website" yaml:"website"`
+	Zap     Zap     `json:"zap" yaml:"zap"`
 }
